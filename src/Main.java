5d9bf2637f79d376620ba9ac0d5354f676c19d5b
mishaf1203 <mishashaferoff@gmail.com>
2022-07-30 06:04:00 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
+    public static Integer[] changeValue(Integer[] value) {
+        value = new Integer[]{1, 2};
+        return value;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer[] value = {3,4};
+        changeValue(value);
+        System.out.println(Arrays.toString(value));
+
     }
 }
